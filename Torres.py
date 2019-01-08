99f1faafbfc09148432c5547211e9efe355f20a4
Gilsiley Daru <ghdaru@gmail.com>
2019-01-08 20:38:54 -0200

    Torre Versão Otimizada

--- Torres.py
+++ Torres.py
@@ -3,14 +3,20 @@ Matriz = [list(map(int,input().split())) for j in range(Ordem)]
 #for i in range(Ordem):
 #    Matriz.append(list(map(int,input().split())))
 #Monta a Matriz soma
+SL = [0 for i in range(Ordem)]
+SC = [0 for i in range(Ordem)]
+#Calcula soma das linhas
+for i in range(Ordem):
+    SL[i] = sum(Matriz[i][:])
+#Calcula soma das colunas
+for j in range(Ordem):
+    SC[j] = sum([Matriz[k][j] for k in range(Ordem)])
+#Varre para encontrar maior valor
 Max = 0
-S = [[0 for i in range(Ordem)] for j in range(Ordem)]
 for i in range(Ordem):
     for j in range(Ordem):
-        somalinha = sum(Matriz[i][:]) - Matriz[i][j]
-        somacoluna = sum([Matriz[k][j] for k in range(Ordem)]) - Matriz[i][j]
-        S[i][j] = somalinha + somacoluna
-        if Max < S[i][j]:
-            Max = S[i][j]
+        Soma = SL[i]+ SC[j] - 2*Matriz[i][j]
+        if Max < Soma:
+            Max = Soma
 print(Max)
 
